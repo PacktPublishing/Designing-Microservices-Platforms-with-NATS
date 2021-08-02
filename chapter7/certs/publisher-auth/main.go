2118cb437957c83a7d675dee5772afdcea56f796
chanakaudaya <chanaka.leadership@gmail.com>
2021-08-02 15:09:20 +0530

    Add authorization samples for chapter7

---
+++ chapter7/certs/publisher-auth/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	// Create a connection to the NATS server over TLS with the RootCA
+	nc, err := nats.Connect("tls://nats.example.com:4222,tls://nats.example.com:5222,tls://nats.example.com:6222", 
+							nats.RootCAs("../ca.pem"), nats.UserInfo("registration_service", "reg123"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Publish a message on "patient.profile" subject
+	subj, msg := "patient.register", []byte("{\"name\":\"parakum\"}")
+	nc.Publish(subj, msg)
+	nc.Flush()
+	if err := nc.LastError(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Published [%s] : '%s'\n", subj, msg)
+	}
+}
+
+
